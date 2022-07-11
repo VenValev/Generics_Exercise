60a43e014c0db1e6a877dd61819bddd959a55689
Vencislav <vencislav67@hotmail.com>
2022-07-11 12:37:12 +0300

    4

--- 01_Box/StartUp.cs
+++ 01_Box/StartUp.cs
@@ -9,15 +9,15 @@ namespace Box
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            var listBox = new List<string>();
+            var listBox = new List<int>();
             for(int i = 0; i < n; i++)
             {
-                string s = Console.ReadLine();
+                int s = int.Parse(Console.ReadLine());
 
                 listBox.Add(s);
             }
 
-            var box = new Box<string>(listBox);
+            var box = new Box<int>(listBox);
 
             int[] indexes = Console.ReadLine()
                 .Split()

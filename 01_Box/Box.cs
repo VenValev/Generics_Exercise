31e410a0ea8d3e918dbdd9e8cca760faa770811e
Vencislav <vencislav67@hotmail.com>
2022-07-11 12:22:53 +0300

    3

--- 01_Box/Box.cs
+++ 01_Box/Box.cs
@@ -6,13 +6,27 @@ namespace Box
 {
     public class Box<T>
     {
+        //Constructors
         public Box(T element)
         {
             Element = element;
         }
+        public Box(List<T> elements)
+        {
+            Elements = elements;
+        }
 
+        //Fields
+        public List<T> Elements { get; }
         public T Element { get; set; }
 
+        //Properties
+        public void Swap(List<T>elements, int in1, int in2)
+        {
+            T firstEle = elements[in1];
+            elements[in1] = elements[in2];
+            elements[in2] = firstEle;
+        }
         public override string ToString()
         {
             return $"{typeof(T)}: {Element}";
